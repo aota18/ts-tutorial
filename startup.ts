a2b9cb55ea418304031ba194b3f682c3457b56e6
aota18 <aotac1v8@gmail.com>
2020-11-18 20:13:03 +0900

    Node Setting Complete

---
+++ startup.ts
@@ -0,0 +1,13 @@
+import * as _ from 'lodash';
+
+class Startup{
+    public static main(): number {
+
+        const group = _.groupBy(['one', 'two', 'three'], length);
+
+        console.log(group);
+        return 0;
+    }
+}
+
+Startup.main();
